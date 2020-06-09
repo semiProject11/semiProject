a1ddfeb0452b4d4d77931acf38dd4f17d01560da
CreativePersonDW <64370784+CreativePersonDW@users.noreply.github.com>
2020-06-09 19:11:07 +0900

    dongwon

--- semiProject/src/filter/CommonFilter.java
+++ semiProject/src/filter/CommonFilter.java
@@ -33,11 +33,11 @@ public class CommonFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
-		request.setCharacterEncoding("utf-8");
-		
-		response.setContentType("text/html; charset=utf-8"); 		
-		
+//		
+//		request.setCharacterEncoding("utf-8");
+//		
+//		response.setContentType("text/html; charset=utf-8"); 		
+//		
 		chain.doFilter(request, response);
 	}
 
